cf4cccab524970bfb4d7dded579cec54483eadb8
Roger1121 <leszczynski_szymon@wp.pl>
2023-12-10 12:30:58 +0100

    Init project and Hellwig implementation

---
+++ lib/MCDA.py
@@ -0,0 +1,18 @@
+import pandas as pd
+class MCDA:
+    @staticmethod
+    def Hellwig(alternatives, ideal_solution, distance_metric):
+        alternatives_df = pd.DataFrame(alternatives)
+        for criterion in alternatives_df.columns:
+            avg = alternatives_df[criterion].mean()
+            std_dev = alternatives_df[criterion].std()
+            alternatives_df[criterion] = (alternatives_df[criterion]-avg)/std_dev
+        synth_vars = {}
+        for i in range(len(alternatives_df)):
+            alternatives_df.at[alternatives_df.index[i], 'di0'] = distance_metric(alternatives_df.at[i], ideal_solution)
+        _d0 = alternatives_df['di0'].sum() / len(alternatives_df)
+        Sd0 = math.sqrt(((alternatives_df['di0'] - _d0) ** 2 / len(alternatives_df)).sum())
+        d0 = _d0 + 2 * Sd0
+        for i in range(len(alternatives_df)):
+            synth_vars[i] = 1 - alternatives_df.at[alternatives_df.index[i], 'di0'] / d0
+        return synth_vars
