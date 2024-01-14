dbff9ae4d17804eecf8ace885a5a950651df6464
Roger1121 <leszczynski_szymon@wp.pl>
2024-01-14 22:06:18 +0100

    Seed database

---
+++ startup.cmd
@@ -0,0 +1,9 @@
+pip install -r requirements.txt
+python manage.py makemigrations mcda
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata ./mcda/seed/problem.json
+python manage.py loaddata ./mcda/seed/criterion.json
+python manage.py loaddata ./mcda/seed/option.json
+python manage.py loaddata ./mcda/seed/criterion-option.json
+python manage.py runserver
