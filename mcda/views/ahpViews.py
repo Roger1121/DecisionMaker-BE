c35f26730d3ef8bd784fa0fc32528f6b77845135
Roger1121 <leszczynski_szymon@wp.pl>
2024-02-25 13:31:07 +0100

    AHP summary

--- mcda/views/ahpViews.py
+++ mcda/views/ahpViews.py
@@ -97,3 +97,94 @@ class OptionComparisonApiView(APIView):
         problem_id = CriterionOption.objects.filter(id=weightsList[0].option_a.id).first().option.problem.id
         SolvingStage.objects.filter(user_id=user_id, problem_id=problem_id).update(stage=3)
         return Response("OK", status=status.HTTP_201_CREATED)
+
+class CriterionMatrixApiView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_user(self, request):
+        try:
+            userToken = request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+            return JwtUtil.get_user(userToken)
+        except:
+            return None
+
+    def get(self, request, *args, **kwargs):
+        user_id = self.get_user(request)
+        if user_id is None:
+            return Response(
+                {"res": "Nie znaleziono użytkownika w bazie"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        problem_id = request.query_params.get('problem_id')
+        if problem_id is None:
+            return Response(
+                {"res": "Nie podano id problemu"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        criteria = Criterion.objects.filter(problem = problem_id)
+        matrix = [[0] * len(criteria)] * len(criteria)
+        for i, criterionA in enumerate(criteria):
+            for j, criterionB in enumerate(criteria):
+                if matrix[i][j] != 0:
+                    continue
+                elif i == j:
+                    matrix[i][j] = {"value": 1, "reversed": False}
+                else:
+                    comparison = CriteriaComparison.objects.filter(user_id=user_id, criterion_a = criterionA.id, criterion_b = criterionB.id)
+                    if len(comparison) == 0:
+                        comparison = CriteriaComparison.objects.filter(user_id=user_id, criterion_a=criterionA.id,
+                                                                       criterion_b=criterionB.id)
+                        matrix[i][j] = {"value": comparison[0].value, "reversed": True}
+                        matrix[i][j] = {"value": comparison[0].value, "reversed": False}
+                    else:
+                        matrix[i][j] = {"value": comparison[0].value, "reversed": False}
+                        matrix[i][j] = {"value": comparison[0].value, "reversed": True}
+        return Response(matrix, status = status.HTTP_200_OK)
+
+class OptionMatrixApiView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_user(self, request):
+        try:
+            userToken = request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+            return JwtUtil.get_user(userToken)
+        except:
+            return None
+
+    def get(self, request, *args, **kwargs):
+        user_id = self.get_user(request)
+        if user_id is None:
+            return Response(
+                {"res": "Nie znaleziono użytkownika w bazie"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        problem_id = request.query_params.get('problem_id')
+        if problem_id is None:
+            return Response(
+                {"res": "Nie podano id problemu"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        criteria = Criterion.objects.filter(problem = problem_id)
+        optionMatrices = []
+        for criterion in criteria:
+            options = CriterionOption.objects.filter(criterion = criterion.id)
+            matrix = [[0] * len(options)] * len(options)
+            for i, optionA in enumerate(options):
+                for j, optionB in enumerate(options):
+                    if matrix[i][j] != 0:
+                        continue
+                    elif i == j:
+                        matrix[i][j] = {"value": 1, "reversed": False}
+                    else:
+                        comparison = OptionComparison.objects.filter(user_id=user_id, option_a=optionA.id,
+                                                                       option_b=optionB.id)
+                        if len(comparison) == 0:
+                            comparison = OptionComparison.objects.filter(user_id=user_id, option_a=optionA.id,
+                                                                           option_b=optionB.id)
+                            matrix[i][j] = {"value": comparison[0].value, "reversed": True}
+                            matrix[i][j] = {"value": comparison[0].value, "reversed": False}
+                        else:
+                            matrix[i][j] = {"value": comparison[0].value, "reversed": False}
+                            matrix[i][j] = {"value": comparison[0].value, "reversed": True}
+            optionMatrices.append({"criterion":criterion.id, "matrix":matrix})
+        return Response(optionMatrices, status = status.HTTP_200_OK)
