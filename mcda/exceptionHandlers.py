564bab2df9aa6cf052bc84aebd38105f4e66ddd1
Roger1121 <leszczynski_szymon@wp.pl>
2024-03-16 17:25:23 +0100

    custom handler for error 500

--- mcda/exceptionHandlers.py
+++ mcda/exceptionHandlers.py
@@ -2,14 +2,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
-
-def handler500(request, *args, **argv):
-    return Response(
-        "Podczas przetwarzania akcji wystąpił nieoczekiwany błąd",
-        status=status.HTTP_400_BAD_REQUEST,
-    )
-
-
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     if exc is not None:
