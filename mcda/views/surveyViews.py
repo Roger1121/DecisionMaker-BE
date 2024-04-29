434e41a06e5392763b665bae90c681a7a62e27e4
Roger1121 <leszczynski_szymon@wp.pl>
2024-04-29 14:42:03 +0200

    Survey fix

--- mcda/views/surveyViews.py
+++ mcda/views/surveyViews.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from mcda.models import SolvingStage, Question, QuestionResponse
 from mcda.jwtUtil import JwtUtil
 
+
 class SurveyApiView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -23,25 +24,26 @@ class SurveyApiView(APIView):
                 "Nie znaleziono użytkownika w bazie",
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        if SolvingStage.objects.filter(user_id = user_id, stage = 3).count() < 2:
+        if SolvingStage.objects.filter(user_id=user_id, stage=3).count() < 2:
             Response(
                 "Ankieta może zostać wypełniona dopiero po ukończeniu dwóch zadań.",
                 status=status.HTTP_200_OK)
         return Response([
             {
-                "question":response.question.id,
+                "question": response.question.id,
                 "content": response.content
-            } for response in QuestionResponse.objects.all()], status=status.HTTP_200_OK)
+            } for response in QuestionResponse.objects.filter(user_id=user_id)], status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         user_id = self.get_user(request)
         if user_id is None:
             return Response("Nie znaleziono użytkownika w bazie",
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+                            status=status.HTTP_400_BAD_REQUEST,
+                            )
         responses = [QuestionResponse(None, user_id, int(resp["question"]), resp["content"]) for resp in request.data]
         QuestionResponse.objects.bulk_create(responses)
         return Response("Dodane odpowiedzi zostały zapisane", status=status.HTTP_201_CREATED)
+
 
 class SurveyAvailableApiView(APIView):
     permission_classes = [IsAuthenticated]
@@ -57,6 +59,6 @@ class SurveyAvailableApiView(APIView):
         user_id = self.get_user(request)
         if user_id is None:
             return Response("Nie znaleziono użytkownika w bazie",
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        return Response(SolvingStage.objects.filter(user_id = user_id, stage = 3).count() == 2, status=status.HTTP_200_OK)
+                            status=status.HTTP_400_BAD_REQUEST,
+                            )
+        return Response(SolvingStage.objects.filter(user_id=user_id, stage=3).count() == 2, status=status.HTTP_200_OK)
